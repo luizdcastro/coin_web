d8959f0da105b425c1055025e960b51a0f2ce9f5
unknown <luizf.dcastro@gmail.com>
2021-09-10 22:16:46 -0300

    update

--- src/components/BotDetails/index.js
+++ src/components/BotDetails/index.js
@@ -12,6 +12,8 @@ import './styles.css'
 const BotDetails = ({ setOpen, botDetails, disptachDeleteBot, disptachUpdateBot, dispatchGetBot, disptachGetMe }) => {
     let moment = require('moment');
     const [orders, setOrders] = useState([])
+    const [updateError, setUpdateError] = useState("")
+    const [logType, setLogtype] = useState("orders")
 
     useEffect(() =>
         dispatchGetBot(
@@ -26,7 +28,7 @@ const BotDetails = ({ setOpen, botDetails, disptachDeleteBot, disptachUpdateBot,
 
     for (const item of orders?.slice(-10)) {
         if (!item.active) {
-            data.push((item.sell_price - item.buy_price).toFixed(2))
+            data.push(item.profit.toFixed(2))
             time.push(moment(item.close_time).format("MMM DD hh:mm A"))
         }
     }
@@ -98,15 +100,21 @@ const BotDetails = ({ setOpen, botDetails, disptachDeleteBot, disptachUpdateBot,
     }
 
     const handleUpdate = (botId, active) => {
-        disptachUpdateBot(
-            active,
-            botId,
-            (response) => {
-                console.log(response);
-                disptachGetMe();
-            },
-            (error) => console.log(error)
-        )
+        if (!!botDetails.settings.exchange.name) {
+            disptachUpdateBot(
+                active,
+                botId,
+                (response) => {
+                    console.log(response);
+                    disptachGetMe();
+                    setOpen(false)
+                },
+                (error) => console.log(error)
+            )
+            setUpdateError("")
+        } else {
+            setUpdateError("Please add an exchange before start your bot.")
+        }
     }
 
     return (
@@ -133,7 +141,7 @@ const BotDetails = ({ setOpen, botDetails, disptachDeleteBot, disptachUpdateBot,
                             </li>
                             <li style={{ display: 'flex' }}>
                                 <p className="bot-model-data-name">Symbol:</p>
-                                <p className="bot-model-data-name_item">{botDetails.settings.symbol.slice(0,-4) + "/" + botDetails.settings.symbol.slice(-4)}</p>
+                                <p className="bot-model-data-name_item">{botDetails.settings.symbol.slice(0, -4) + "/" + botDetails.settings.symbol.slice(-4)}</p>
                             </li>
                             <li style={{ display: 'flex' }}>
                                 <p className="bot-model-data-name">Exchange:</p>
@@ -154,7 +162,7 @@ const BotDetails = ({ setOpen, botDetails, disptachDeleteBot, disptachUpdateBot,
                                     <FiIcons.FiPause size={17} style={{ marginRight: 5 }} />
                                     <p className="bot-model-icon-name">Pause</p>
                                 </li>
-                                : <li className="bot-model-icon-container" onClick={() => { handleUpdate(botDetails.id, true); setOpen(false) }}>
+                                : <li className="bot-model-icon-container" onClick={() => { handleUpdate(botDetails.id, true) }}>
                                     <FiIcons.FiPlay size={17} style={{ marginRight: 5 }} />
                                     <p className="bot-model-icon-name">Start</p>
                                 </li>
@@ -169,51 +177,76 @@ const BotDetails = ({ setOpen, botDetails, disptachDeleteBot, disptachUpdateBot,
                             </li>
                         </ul>
                     </div>
+                    {!!updateError && (<p className="bot_model-error">{updateError}</p>)}
+
                     <div width="100%" height={300}>
                         <Line data={chartData} options={options} />
                     </div>
-                    <h3 className="bot-modal-order_title">Orders History</h3>
-                    {orders.length >= 1 && (
-                        orders.map((item) => (
-                            <div key={item.id} className="bot-model-order_container">
-                                <ul className="bot-modal-order_list">
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Order Status:</p>
-                                        <p className="bot-model-data-name_item">{item.active ? "Open" : "Closed"}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Open Time:</p>
-                                        <p className="bot-model-data-name_item">{moment(item.open_time).format("MM-DD-YYYY hh:mm A")}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Quantity:</p>
-                                        <p className="bot-model-data-name_item">{item.quantity?.toFixed(4)}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Buy Price:</p>
-                                        <p className="bot-model-data-name_item">{item.buy_price?.toFixed(2)}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Close Time:</p>
-                                        <p className="bot-model-data-name_item">{item.close_time && (moment(item.close_time).format("MM-DD-YYYY hh:mm A"))}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Sell Price:</p>
-                                        <p className="bot-model-data-name_item">{item.sell_price?.toFixed(2)}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Percent:</p>
-                                        <p className="bot-model-data-name_item">{item.percent?.toFixed(2)}</p>
-                                    </li>
-                                    <li style={{ display: 'flex' }}>
-                                        <p className="bot-model-data-name">Profit:</p>
-                                        <p className="bot-model-data-name_item">{item.profit?.toFixed(2)}</p>
-                                    </li>
-                                </ul>
-                                <div className="bot-modal-order_percent">
+                    <div style={{ display: 'flex', justifyContent: 'center', marginBottom: 20, marginTop: 20 }}>
+                        <button
+                            className={logType === "orders" ? "bot-modal_orders-button-active" : "bot-modal_orders-button"}
+                            onClick={() => setLogtype("orders")}
+                        >Orders History</button>
+                        <button
+                            className={logType === "events" ? "bot-modal_orders-button-active" : "bot-modal_orders-button"}
+                            onClick={() => setLogtype("events")}
+                        >Event logs</button>
+                    </div>
+                    {logType === "orders" ?
+                        <div>
+                            {!!orders.length ?
+                                orders.map((item) => (
+                                    <div key={item.id} className="bot-model-order_container">
+                                        <ul className="bot-modal-order_list">
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Order Status:</p>
+                                                <p className="bot-model-data-name_item">{item.active ? "Open" : "Closed"}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Open Time:</p>
+                                                <p className="bot-model-data-name_item">{moment(item.open_time).format("MM-DD-YYYY hh:mm A")}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Quantity:</p>
+                                                <p className="bot-model-data-name_item">{item.quantity?.toFixed(4)}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Buy Price:</p>
+                                                <p className="bot-model-data-name_item">{item.buy_price?.toFixed(2)}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Close Time:</p>
+                                                <p className="bot-model-data-name_item">{item.close_time && (moment(item.close_time).format("MM-DD-YYYY hh:mm A"))}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Sell Price:</p>
+                                                <p className="bot-model-data-name_item">{item.sell_price?.toFixed(2)}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Percent:</p>
+                                                <p className="bot-model-data-name_item">{!!item.percent ? item.percent?.toFixed(2) + "%" : ""}</p>
+                                            </li>
+                                            <li style={{ display: 'flex' }}>
+                                                <p className="bot-model-data-name">Total:</p>
+                                                <p className="bot-model-data-name_item">{!!item.profit ? "$" + item.profit.toFixed(2) : ""}</p>
+                                            </li>
+                                        </ul>
+                                        <div className="bot-modal-order_percent">
+                                        </div>
+                                    </div>
+                                ))
+                                :
+                                <div style={{ marginTop: 35 }}>
+                                    <p style={{ fontWeight: 500, color: 'rgba(255,255,255, 0.8)', fontSize: 18, textAlign: 'center', marginBottom: 10 }}>No orders were found.</p>
+                                    <p style={{ fontWeight: 400, color: 'rgba(255,255,255, 0.8)', fontSize: 14, textAlign: 'center' }}>Waiting for condition logic to be triggered.</p>
                                 </div>
-                            </div>
-                        )))}
+                            }
+                        </div>
+                        :
+                        <div style={{ marginTop: 35 }}>
+                            <p style={{ fontWeight: 500, color: 'rgba(255,255,255, 0.8)', fontSize: 18, textAlign: 'center', marginBottom: 10 }}>No events were found.</p>
+                            <p style={{ fontWeight: 400, color: 'rgba(255,255,255, 0.8)', fontSize: 14, textAlign: 'center' }}>Any events or errors were tracked so far.</p>
+                        </div>}
                 </div>
             </div>
         </React.Fragment>
